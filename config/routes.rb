1aeb83afb02cfee8a77760fbc00aad8249dcdbc0
Perchitti <LaurenPerchitti@gmail.com>
2018-06-22 15:50:23 -0400

    Routes updated.

--- config/routes.rb
+++ config/routes.rb
@@ -13,5 +13,6 @@ post '/logout', to: 'sessions#destroy'
 
 
 resources :projects
+resources :users
 end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
