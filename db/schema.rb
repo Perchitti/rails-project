39a4a0aa7dda8c802818ee49321441359ca8820f
Perchitti <LaurenPerchitti@gmail.com>
2018-06-28 12:51:43 -0400

    Updated Schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_06_19_190934) do
+ActiveRecord::Schema.define(version: 2018_06_28_162753) do
+
+  create_table "notes", force: :cascade do |t|
+    t.string "content"
+    t.integer "user_id"
+    t.integer "project_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["project_id"], name: "index_notes_on_project_id"
+    t.index ["user_id"], name: "index_notes_on_user_id"
+  end
 
   create_table "projects", force: :cascade do |t|
     t.string "title"
@@ -25,6 +35,8 @@ ActiveRecord::Schema.define(version: 2018_06_19_190934) do
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "provider"
+    t.string "uid"
   end
 
 end
