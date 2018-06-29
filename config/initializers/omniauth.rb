c065c02bf40ab5dacc53e92b236ec693c814709c
Perchitti <LaurenPerchitti@gmail.com>
2018-06-29 12:56:40 -0400

    OAuth google client and secret

---
+++ config/initializers/omniauth.rb
@@ -0,0 +1,3 @@
+Rails.application.config.middleware.use OmniAuth::Builder do
+  provider :google_oauth2, "646916027859-lfrc6do8jks8m5emmekhg3h41ts6t09t.apps.googleusercontent.com", "o130e0fjXhWXrlmGJ0foyNfz"
+end
