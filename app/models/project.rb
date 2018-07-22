d570d050d7c60eb5d374081ad773c086e12ada52
Lauren Perchitti <31042804+Perchitti@users.noreply.github.com>
2018-07-22 17:27:45 -0400

    Added Nested Routes

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,6 +1,5 @@
 class Project < ApplicationRecord
   belongs_to :user
-  has_many :notes
 
   validates :title, presence: true
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
 root 'users#index'
+resources :users, only: [:show] do
+  resources :projects
+end
 
-resources :projects
-resources :users
 get '/', to: 'users#index', as: 'login'
  get '/logout', to: 'sessions#destroy', as: 'logout'
 get 'auth/google_oauth2/callback', to: 'sessions#create'
@@ -11,8 +12,10 @@ get '/signup', to: 'users#new'
 post '/signup', to: 'users#create'
 get '/show', to: 'users#show'
 post '/logout', to: 'sessions#destroy'
+patch '/users/:user_id/projects', to: 'projects#update'
 delete '/logout', to: 'sessions#destroy'
-
+resources :projects
+resources :users
 
 
 
